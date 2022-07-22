447c1d51fb46e3c61d924e15fc595327562825b1
Your Name <you@example.com>
2022-07-22 19:06:08 +0900

    20200722저녁

--- cocoa/core/settings.py
+++ cocoa/core/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
     'widget_tweaks',
 
     'tesseract_ocr', 
